db01a8576a6e5b051f5af6821fe1e0331e1fa68a
llamousse <vyue013@gmail.com>
2022-10-15 14:48:07 -0700

    add board 2d layout

--- src/Board.js
+++ src/Board.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Board.css";
 
-function Board() {
-  return <div>Board</div>;
-}
+const BOARD_SIZE = 10;
+
+const Board = () => {
+  const [board, setBoard] = useState(
+    new Array(BOARD_SIZE).fill(0).map((row) => new Array(BOARD_SIZE).fill(0))
+  );
+
+  return (
+    <div className="board">
+      {board.map((row, rowIdx) => (
+        <div key={rowIdx} className="row">
+          {row.map((cell, cellIdx) => (
+            <div key={cellIdx} className="cell"></div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Board;
