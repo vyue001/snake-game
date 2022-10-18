80f7ab955814c4d06d802a1f1e59f1535b2023a1
vyue.20 <vyue001@ucr.edu>
2022-10-17 17:33:46 -0700

    comments on functions and how they work

--- src/Board.js
+++ src/Board.js
@@ -6,13 +6,15 @@ const BOARD_SIZE = 10;
 const Board = () => {
   const [board, setBoard] = useState(
     new Array(BOARD_SIZE).fill(0).map((row) => new Array(BOARD_SIZE).fill(0))
-  );
+  ); // default board state is an array of 10 length 
 
   return (
     <div className="board">
+    // map board with 10x10 dimesions
       {board.map((row, rowIdx) => (
         <div key={rowIdx} className="row">
           {row.map((cell, cellIdx) => (
+            // the color that will be shown depending on snake or food cell
             <div key={cellIdx} className={`cell ${true ? 'snake-cell' : ''}`}></div>
           ))}
         </div>
